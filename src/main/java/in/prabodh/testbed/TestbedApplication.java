874d41f0e04943d5407244b7dc86c44e565585a8
prabodh-dev1 <developer1@prabodh.in>
2022-03-26 06:21:41 +0530

    updated package structure, and consolidated code as single application

--- src/main/java/in/prabodh/testbed/TestbedApplication.java
+++ src/main/java/in/prabodh/testbed/TestbedApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"in.prabodh"})
 @EntityScan(basePackages={"in.prabodh"})
+@EnableJpaRepositories({"in.prabodh.repo"})
 public class TestbedApplication {
 
 	public static void main(String[] args) {
